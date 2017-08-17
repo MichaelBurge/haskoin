e8840788388011eeae8782836e0eceac14654474
Michael Burge <michaelburge@pobox.com>
2017-08-17 10:46:09 -0700

    Initial commit of some basic types

--- src/Haskoin/Types.hs
+++ src/Haskoin/Types.hs
@@ -1,4 +1,4 @@
-{-# LANGUAGE NoImplicitPrelude, DeriveTraversable, DeriveDataTypeable, StandaloneDeriving, TypeSynonymInstances, FlexibleInstances #-}
+{-# LANGUAGE GeneralizedNewtypeDeriving, NoImplicitPrelude, DeriveTraversable, DeriveDataTypeable, StandaloneDeriving, TypeSynonymInstances, FlexibleInstances #-}
 
 module Haskoin.Types where
 
@@ -9,7 +9,7 @@ import Control.Comonad.Cofree
 import Data.Data
 import qualified Data.Vector as V
 
-newtype Account = Account Integer deriving (Eq, Show)
+newtype Account = Account Integer deriving (Eq, Show, Num)
 
 data Transaction = Transaction {
   _from   :: Account,
@@ -17,11 +17,18 @@ data Transaction = Transaction {
   _amount :: Integer
   } deriving (Eq, Show)
 
-newtype Block = Block (V.Vector Transaction) deriving (Eq, Show)
+newtype BlockF a = Block (V.Vector a) deriving (Eq, Show, Foldable, Traversable, Functor, Monoid)
+type Block = BlockF Transaction
+
 type HaskoinHash = Digest SHA1
 
+data BlockHeader = BlockHeader {
+  _miner       :: Account,
+  _parentHash  :: HaskoinHash
+  } deriving (Eq, Show)
+
 data MerkleF a = Genesis
-               | Node HaskoinHash [a]
+               | Node BlockHeader a
                deriving (Eq, Show, Functor, Traversable, Foldable)
 
 type Blockchain = Cofree MerkleF Block
