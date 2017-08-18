bc2495492e79f7ed58b6e2b63e4d4a3514ea1880
Michael Burge <michaelburge@pobox.com>
2017-08-17 20:00:23 -0700

    Command tool to generate a blockchain

---
+++ app/New.hs
@@ -0,0 +1,3 @@
+import qualified Haskoin.Cli.New as New
+
+main = New.main
---
+++ src/Haskoin/Cli/New.hs
@@ -0,0 +1,23 @@
+{-# LANGUAGE NoImplicitPrelude, OverloadedStrings #-}
+
+module Haskoin.Cli.New where
+
+import Haskoin.Mining
+import Haskoin.Serialization
+import Haskoin.Types
+
+import Protolude
+import System.Environment
+import Data.Binary
+import qualified Data.ByteString.Lazy as BSL
+
+defaultChainFile = "main.chain"
+
+main = do
+  args <- getArgs
+  let filename = case args of
+        [] -> defaultChainFile
+        [x] -> x
+        _ -> panic "Usage: new [filename]"
+  chain <- makeGenesis
+  BSL.writeFile filename $ encode chain
